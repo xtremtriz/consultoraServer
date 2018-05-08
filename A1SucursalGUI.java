f3f05daf5087f19d50ced2ad1a217eb8fcda32a0
xtrem <xtrem_triz@live.com>
2018-05-07 23:58:10 -0500

    remoto

--- A1SucursalGUI.java
+++ A1SucursalGUI.java
@@ -11,6 +11,7 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.StringTokenizer;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class A1SucursalGUI extends JFrame implements ActionListener {
@@ -18,9 +19,10 @@ public class A1SucursalGUI extends JFrame implements ActionListener {
     private JTextField tfNombre, tfDireccion, tfNumSucursal, tfTelefono;
     private JPanel panel1, panel2;
     private JTextArea taDatos;
+    private StringTokenizer st;
 
     private CompanyADjdbc companyad = new CompanyADjdbc();
-    
+    private Conexion conexion = new Conexion();
 
     public A1SucursalGUI() {
         super("Asignacion de Sucursal");
@@ -65,6 +67,15 @@ public class A1SucursalGUI extends JFrame implements ActionListener {
         setSize(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+    private void tokenizar(String datos){
+        String token = "";
+        st = new StringTokenizer(datos,"*");
+        while(st.hasMoreTokens()){
+            token = token + st.nextToken() + '\n';
+            System.out.println(token);
+            taDatos.setText(token);
+        }
+    }
 
     public JPanel getPanel2() {
         return this.panel2;
@@ -100,32 +111,53 @@ public class A1SucursalGUI extends JFrame implements ActionListener {
     
 
     public void actionPerformed(ActionEvent e) {
-        //String datos = "";
+        String datos = "";
         if(e.getSource() == bCapturar)
 		{
-			String datos="";
-			String resultado="";
+			
+			String respuesta="";
 			
 			// 1. Obtner dato de los JTextFields
 			datos = obtenerDatos();
 			
 			// 2. Checar si algun campo es vacio o saldo no numerico
 			if(datos.equals("vacio"))
-				taDatos.setText("Algun campo esta vacio...");
+				respuesta = "Algun campo esta vacio...";
 			
 			else{
+                                // 2.1 Establecer conexion con el server
+                                conexion.establecerConexion();
+                                
+                                // 2.2 Enviar la transaccion a realizar
+                                conexion.enviarDatos("AltaSucursal");
 			
-				// 3. Capturar los datos del cliente
-				resultado = companyad.AltaSucursal(datos);
+				// 2.3 Enviar los datos a capturar en la DB
+                                conexion.enviarDatos(datos);
 				
-				// 4. Desplegar resultado de la transaccion
-				taDatos.setText(resultado);
+				// 2.4 Recibir resultado de la transaccion
+                                respuesta = conexion.recibirDatos();
+                                
+                                // 2.5 Cerrar la conexion
+                                conexion.cerrarConexion();
 			}
+                        // 3. Desplegar resultado de la transaccion
+                        taDatos.setText(respuesta);
 		}
 
         if (e.getSource() == bConsultar) { 
             //System.out.println("Entra");
-            String datos = companyad.consultarSucursales();
+            // 1. Establecer conexion con el Server
+            conexion.establecerConexion();
+            // 2. Enviar transaccion a realizar
+            conexion.enviarDatos("consultarSucursales");
+            // 3. Recibir resultado de la transaccion
+            datos = conexion.recibirDatos();
+            // 4. Cerrrar conexion
+            conexion.cerrarConexion();
+            // 5. Desplegar datos
+            // taDatos.setText(datos);
+            tokenizar(datos);
+            //String datos = companyad.consultarSucursales();
             if(datos.isEmpty()){
                 datos = "Datos vacios";
             }            
---
+++ Conexion.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Uriel
+ */
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+
+import java.io.IOException;
+
+import java.net.Socket;
+
+public class Conexion extends JFrame {
+    private JTextField tfDatos;
+    private JButton bEnviar;
+    private JTextArea taDatos;
+    private JPanel panel1;
+    private JPanel panel2;
+
+    private Socket socket;
+
+    private BufferedReader bufferEntrada;
+    private PrintWriter bufferSalida;
+
+    // Constructor
+    public Conexion() {
+        super("Conexion");
+
+        // 1. Crear los objetos de los atributos
+        tfDatos = new JTextField();
+        taDatos = new JTextArea();
+        bEnviar = new JButton("Enviar datos");
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+
+        // 2. Definir el Layout de los panels
+        panel1.setLayout(new GridLayout(1, 2));
+        panel2.setLayout(new BorderLayout());
+
+        // 4. Adicionar los objetos a los panels
+        panel1.add(tfDatos);
+        panel1.add(bEnviar);
+
+        panel2.add(panel1, BorderLayout.NORTH);
+        panel2.add(new JScrollPane(taDatos), BorderLayout.CENTER);
+
+        // 5. Adicionar panel2 al JFrame y hacerlo visible
+        add(panel2);
+        setSize(400, 400);
+        // setVisible(true);
+    }
+
+    public void enviarDatos(String datos) {
+        bufferSalida.println(datos);
+        bufferSalida.flush();
+    }
+
+    public String recibirDatos() {
+        String datos = "";
+
+        try {
+            datos = bufferEntrada.readLine();
+            bufferSalida.flush();
+        } catch (IOException ioe) {
+            System.out.println("Error: " + ioe);
+        }
+
+        return datos;
+    }
+
+    public void establecerConexion(){
+        String mensaje = "";
+
+        try {
+                // 1. Establecer conexion con el server en el puerto 5005
+                socket = new Socket("localhost",5005);
+                //taDatos.append("\nSe hizo la Conexion con el Server...\n");
+
+                // 2. Preparar canales o buffers de comunicacion
+                bufferEntrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                bufferSalida = new PrintWriter(socket.getOutputStream());
+                bufferSalida.flush();
+                //taDatos.append("Mensaje del Server: " + mensaje);
+        } catch (IOException ioe) {
+            System.out.println("Error: " + ioe);
+        }
+    }
+
+    public void cerrarConexion() {
+        try {
+            bufferEntrada.close();
+            bufferSalida.close();
+            socket.close();
+        } catch (IOException ioe) {
+            System.out.println("Error: " + ioe);
+        }
+    }
+
+}
